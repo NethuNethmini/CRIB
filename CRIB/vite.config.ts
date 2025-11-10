5729cd89bae73aeafe1f3878bab71c85bec0c6c1
HETTIARACHCHI H.A.D.A.S. <asindu@futurecx.com>
2025-11-10 14:29:33 +0530

    make changes

--- CRIB/vite.config.ts
+++ CRIB/vite.config.ts
@@ -5,8 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server:{
-    allowedHosts: ['crib.becx.io']
+  server: {
+    allowedHosts: ['crib.becx.io'],
+    host: "0.0.0.0",
+    port: 5173,
+    open: true,
   }
 
 })
